f12f61111c7ca0dd173cb85de933e290db2a8fc1
Jonathan Cheng <joncheng.dev@gmail.com>
2023-08-21 14:23:17 -0700

    pass 1st test for ToDoList.prototype.addTask method

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,12 @@ function Task(name, completionStatus) {
     this.taskName = name;
     this.completed = completionStatus;
 }
+
+ToDoList.prototype.addTask = function(newTask) {
+    this.tasks[newTask.taskName] = newTask;
+}
+
+let todaysChores = new ToDoList();
+
+let taskOne = new Task("Go to the park", false);
+let taskTwo = new Task("Go to the mall", false);
